0575dc0378b197d2438fcf8013a34f09a06e2fd0
adil_el_gham <adil.el.ghamarti@student.ehb.be>
2021-01-18 18:34:09 +0100

    making basic website with privacy verklaring

---
+++ web-app/resources/views/partials/header_partial.blade.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="{{route('welcome')}}">Home<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('privacy')}}">Privacy<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">Events<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">Contact<span class="sr-only">(current)</span></a>
+            </li>
+        </ul>
+    </div>
+</nav>
